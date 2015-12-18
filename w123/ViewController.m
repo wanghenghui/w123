381e937f471365cf87d7a2fe180c8e9a22141a11
wanghenghui <wang1551ios@sina.com>
2015-12-18 10:56:34 -0800

    Update ViewController.m

--- w123/ViewController.m
+++ w123/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.a
+    //修改一下
     self.view.backgroundColor = [UIColor redColor];
 }
 
