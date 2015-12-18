6c37b7f429b4022c230ee39d35d8086540d6ae42
wanghenghui <whh@whhdeMac.local>
2015-12-18 10:59:20 -0800

    1
    2

---
+++ w123/whh.h
@@ -0,0 +1,13 @@
+//
+//  whh.h
+//  w123
+//
+//  Created by whh on 15/12/18.
+//  Copyright © 2015年 whh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface whh : NSObject
+
+@end
---
+++ w123/whh.m
@@ -0,0 +1,13 @@
+//
+//  whh.m
+//  w123
+//
+//  Created by whh on 15/12/18.
+//  Copyright © 2015年 whh. All rights reserved.
+//
+
+#import "whh.h"
+
+@implementation whh
+
+@end
